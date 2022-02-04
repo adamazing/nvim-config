66392e10e97f7886ccf6d0064cc183c8459e2695
Adam Henley <adamazing@gmail.com>
2022-02-04 22:50:21 +1300

    Config TabNine
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,16 +1,5 @@
 -- set up cmp_tabnine
-local tabnine = require('cmp_tabnine.config')
-tabnine:setup({
-	max_lines = 1000;
-	max_num_results = 20;
-	sort = true;
-	run_on_every_keystroke = true;
-	snippet_placeholder = '..';
-	ignored_file_types = { -- default is not to ignore
-		-- uncomment to ignore in lua:
-		-- lua = true
-	};
-})
+local compare = require('cmp.config.compare')
 
 local source_mapping = {
 	buffer = "[Buffer]",
@@ -109,7 +98,21 @@ cmp.setup({
     { name = 'vsnip' },
     { name = 'buffer' },
     { name = 'path' },
-  })
+  }),
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require('cmp_tabnine.compare'),
+      compare.offset,
+      compare.exact,
+      compare.score,
+      compare.recently_used,
+      compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+    }
+  }
 })
 cmp.setup.cmdline('/', {
   sources = {
@@ -124,4 +127,17 @@ cmp.setup.cmdline(':', {
   }, {
     { name = 'cmdline' }
   })
+})
+
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+	max_lines = 1000;
+	max_num_results = 20;
+	sort = true;
+	run_on_every_keystroke = true;
+	snippet_placeholder = '..';
+	ignored_file_types = { -- default is not to ignore
+		-- uncomment to ignore in lua:
+		-- lua = true
+	};
 })
