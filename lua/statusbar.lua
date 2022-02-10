c9b96621bbf3fd2d4e53b163feec7a470c90e565
Adam Henley <adam.henley@flickelectric.co.nz>
2022-02-10 23:58:23 +1300

    Tweak status bar
    Signed-off-by: Adam Henley <adam.henley@flickelectric.co.nz>

--- lua/statusbar.lua
+++ lua/statusbar.lua
@@ -2,30 +2,48 @@ local function setup_status_line()
   require'lualine'.setup {
     options = {
       icons_enabled = true,
-      theme = 'gruvbox',
-      component_separators = {'', ''},
-      section_separators = {'', ''},
+      theme = 'auto',
+      component_separators = {left = '', right = ''},
+      section_separators = {left = '', right = ''},
       disabled_filetypes = {}
     },
     sections = {
       lualine_a = {'mode'},
-      lualine_b = {'branch'},
-      lualine_c = {'filename'},
-      lualine_x = {'diff', 'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_b = {'branch', 'diagnostics'},
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+        }
+      },
+      lualine_x = {
+        'diff',
+        'encoding',
+        'fileformat',
+        {
+          'filetype',
+          colored = true,
+          icon_only = false,
+        }
     },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
-      lualine_y = {},
-      lualine_z = {}
-    },
-    tabline = {},
-    extensions = {}
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {
+    'fugitive',
+    'nvim-tree',
+    'symbols-outline'
   }
+}
 end
 
 setup_status_line()
