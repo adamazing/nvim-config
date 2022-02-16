52867ea87866536a4f3f400a83ac75f347ce373a
Adam Henley <adam.henley@flickelectric.co.nz>
2022-02-17 11:49:21 +1300

    Change mapping for signature help
    Signed-off-by: Adam Henley <adam.henley@flickelectric.co.nz>

--- lua/lang/init.lua
+++ lua/lang/init.lua
@@ -52,7 +52,7 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   local opts = {noremap = true, silent = true}
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<leader>gk', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
